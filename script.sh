61eeb73b1d6ec0ffccb27f582970ec51e77f6f58
Lorenzo Bugli <bugli.lorenzo@gmail.com>
2020-10-08 20:01:26 +0200

    updated script

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-docker exec mysql -u root -e "cretate database testfunzionamento;"
-docker exec mysql -u root -e "cretate database pmremote;"
-docker exec test_lamp mysql -u root testfunzionamento < testfunzionamento.sql 
-docker exec test_lamp mysql -u root pmremote < pmremote.sql 
+docker exec -w /var/www/html -d test_lamp mysql -u root -e "create database testfunzionamento;"
+docker exec -w /var/www/html -d test_lamp mysql -u root -e "create database pmremote;"
+docker exec -w /var/www/html -d test_lamp mysql -u root testfunzionamento < testfunzionamento.sql 
+docker exec -w /var/www/html -d test_lamp mysql -u root pmremote < pmremote.sql 
